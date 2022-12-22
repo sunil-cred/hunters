0e894124e9765aab32e6661e5feb5ed5ab856477
sunil-cred <sunil.kumar@credgenics.com>
2022-12-22 20:25:24 +0530

    add models user and loan details

--- app/models/models.py
+++ app/models/models.py
@@ -1,26 +1,38 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String,TIMESTAMP,BIGINT,DATE,ColumnDefault,FLOAT
 from sqlalchemy.orm import relationship
 
 from app.database import Base
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "users_details"
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
+    id = Column(BIGINT, primary_key=True, index=True,autoincrement=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    mobile = Column(BIGINT)
+    email =  Column(String)
+    date_of_birth = Column(DATE)
+    created_at = Column(TIMESTAMP)
+    updated_at =Column(TIMESTAMP,ColumnDefault("now()"))
+    is_deleted = Column(Boolean,ColumnDefault(False))
 
-    items = relationship("Item", back_populates="owner")
 
+class LoanDetails(Base):
+    __tablename__ = "loan_account_details"
 
-class Item(Base):
-    __tablename__ = "items"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
-
-    owner = relationship("User", back_populates="items")
+    id = Column(BIGINT, primary_key=True, index=True, autoincrement=True)
+    loan_id = Column(String)
+    name = Column(String)
+    mobile = Column(BIGINT)
+    email = Column(String)
+    loan_sanctioned_amount = Column(FLOAT)
+    loan_tenure = Column(Integer)
+    rate_of_interest = Column(FLOAT)
+    emi_amount = Column(FLOAT)
+    emi_type = Column(String)
+    emi_date = Column(DATE)
+    created_at = Column(TIMESTAMP)
+    created_by = Column(String)
+    updated_at = Column(TIMESTAMP)
+    updated_by = Column(String)
--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ from os import getenv
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
-DATABASE_URL = getenv("DATABASE_URL")
+DATABASE_URL = getenv("DATABASE_URL","")
 MODULE_NAME = getenv("MODULE_NAME","/hunters")
 BASE_ROUTE = getenv("BASE_ROUTE","/hunters")
 LOG_LEVEL = getenv("LOG_LEVEL", "INFO")
