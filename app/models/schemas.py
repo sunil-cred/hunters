6b9509c90c7de9aa30d0ef172d53facc64032fc0
Chirag Kumawat <43013693+ChiragIITK@users.noreply.github.com>
2022-12-23 01:51:26 +0530

    dummy data file added

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -2,4 +2,3 @@ from pydantic import BaseModel
 
 class UserLogin(BaseModel):
     mobile: int
-
---
+++ app/routes/test_routes.py
@@ -0,0 +1,14 @@
+from ..constants import *
+from fastapi import APIRouter
+from app.database import SessionLocal
+
+router = APIRouter()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
