60a90927c3af6b0ba3bd7625d8d342b875185031
sunil-cred <sunil.kumar@credgenics.com>
2022-12-22 19:51:51 +0530

    add database

--- server.py
+++ server.py
@@ -3,9 +3,8 @@ import logging
 from app.routes.test_routes import router as test_router
 from settings import *
 from app.util.auth import get_service_permissions
-from elasticapm.contrib.starlette import make_apm_client, ElasticAPM
 from app.models import models
-from app.database import SessionLocal, engine, Base
+from app.database import engine
 
 extra = {"app_name": MODULE_NAME}
 
@@ -19,9 +18,8 @@ logger = logging.getLogger(__name__)
 logger = logging.LoggerAdapter(logger, extra)
 
 
-apm = make_apm_client(ELASTIC_APM)
+
 app = FastAPI()
-app.add_middleware(ElasticAPM, client=apm)
 app.include_router(test_router, tags=["test"], prefix=f"{BASE_ROUTE}")
 
 
--- settings.py
+++ settings.py
@@ -3,20 +3,6 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 DATABASE_URL = getenv("DATABASE_URL")
-MODULE_NAME = getenv("MODULE_NAME")
-BASE_ROUTE = getenv("BASE_ROUTE")
+MODULE_NAME = getenv("MODULE_NAME","/hunters")
+BASE_ROUTE = getenv("BASE_ROUTE","/hunters")
 LOG_LEVEL = getenv("LOG_LEVEL", "INFO")
-
-ELASTIC_APM = {
-    "SERVICE_NAME": getenv("SERVICE_NAME"),
-    "SECRET_TOKEN": getenv("APM_SECRET_TOKEN"),
-    "SERVER_URL": getenv("APM_SERVER_URL"),
-    "ENVIRONMENT": getenv("ENV"),
-    "LOG_LEVEL": getenv("LOG_LEVEL", "INFO"),
-    "VERIFY_SERVER_CERT": bool(getenv("APM_VERIFY_SERVER_CERT", "False")),
-    "TRANSACTION_IGNORE_URLS": ["*/healthz"],
-    "LOCAL_VAR_DICT_MAX_LENGTH": int(getenv("APM_LOCAL_VAR_DICT_MAX_LENGTH", "25")),
-    "LOCAL_VAR_MAX_LENGTH": int(getenv("APM_LOCAL_VAR_MAX_LENGTH", "1000")),
-    "CAPTURE_BODY": getenv("APM_CAPTURE_BODY", "off"),
-    "SPAN_STACK_TRACE_MIN_DURATION": getenv("APM_CAPTURE_BODY", 0),
-}
