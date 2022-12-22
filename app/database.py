9df55d7b1ec30f9a460a523b61f2ea3bc609fa60
Chirag Kumawat <43013693+ChiragIITK@users.noreply.github.com>
2022-12-23 02:37:41 +0530

    dummy data script

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from settings import *
+from settings import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
