0e30f77a67c42a5f876f9dd61826e2035968600f
sunil-cred <sunil.kumar@credgenics.com>
2022-12-23 01:34:13 +0530

    added more tables

--- app/models/models.py
+++ app/models/models.py
@@ -32,7 +32,57 @@ class LoanDetails(Base):
     emi_amount = Column(FLOAT)
     emi_type = Column(String)
     emi_date = Column(DATE)
+    enach_enabled = Column(Boolean)
+    status = Column(String)
     created_at = Column(TIMESTAMP)
     created_by = Column(String)
     updated_at = Column(TIMESTAMP)
     updated_by = Column(String)
+    is_deleted = Column(Boolean,ColumnDefault(False))
+
+class Transactions(Base):
+    __tablename__ = "transactions"
+
+    id = Column(BIGINT, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(BIGINT)
+    account_id = Column(BIGINT)
+    lender_id = Column(BIGINT)
+    amount = Column(FLOAT)
+    repayment_date = Column(DATE)
+    payment_type = Column(String)
+    status = Column(String)
+    created_at = Column(TIMESTAMP)
+    updated_at = Column(TIMESTAMP)
+
+class LenderDetails(Base):
+    __tablename__ = "lender_details"
+
+    id = Column(BIGINT, primary_key=True, index=True, autoincrement=True)
+    name = Column(String)
+    payment_link = Column(String)
+    enable_pay_now = Column(Boolean)
+    enable_recurring = Column(Boolean)
+    enable_one_time = Column(Boolean)
+    settlement_percentage = Column(Integer)
+    created_at = Column(TIMESTAMP)
+    created_by = Column(String)
+    Updated_at = Column(TIMESTAMP)
+    Updated_by = Column(String)
+    is_deleted = Column(Boolean)
+
+class DocumentDetails(Base):
+    __tablename__ = "document_details"
+
+    id = Column(BIGINT, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(BIGINT)
+    account_id = Column(BIGINT)
+    document_type = Column(String)
+    document_link = Column(String)
+    verified_by = Column(String)
+    verified_at = Column(TIMESTAMP)
+    verification_status = Column(String)
+    created_at = Column(TIMESTAMP)
+    created_by = Column(String)
+    Updated_at = Column(TIMESTAMP)
+    Updated_by = Column(String)
+    is_deleted = Column(Boolean)
